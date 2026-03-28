6337f3e5ae4dfe9539fba716c025c273f310670e
EdizCinbas <oecinbas@gmail.com>
2026-03-28 11:33:52 +0000

    Frontend initial commit

---
+++ frontend/src/components/GraphPanel.tsx
@@ -0,0 +1,12 @@
+export default function GraphPanel() {
+  return (
+    <aside className="panel panel--graph">
+      <div className="panel__header">
+        <span className="panel__title">Chart</span>
+      </div>
+      <div className="panel__body panel__body--empty">
+        {/* graph goes here */}
+      </div>
+    </aside>
+  )
+}
